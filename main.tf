1ce0993fa5c4cff5fdab5909c2dc980def7b3f1f
Ming Xuan Wu <32241723+moodyloo@users.noreply.github.com>
2024-02-28 21:56:46 +0000

    -

--- main.tf
+++ main.tf
@@ -50,13 +50,13 @@ module "autoscaling" {
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  subnet_id     = module.blog_vpc.public_subnets[0]
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Name = "Blog"
+    Name = "Learning Terraform"
   }
 }
 
