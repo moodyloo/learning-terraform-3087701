9894740c7e23bb9b241b69454e3848299a06c2f7
Ming Xuan Wu <32241723+moodyloo@users.noreply.github.com>
2024-02-28 13:41:55 +0000

    -

--- main.tf
+++ main.tf
@@ -52,16 +52,27 @@ module "blog_alb" {
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
-  
-
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
+ 
+  listeners = {
+  ex-http-https-redirect = {
+    port     = 80
+    protocol = "HTTP"
+    redirect = {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
     }
-  ]
+  }
+  ex-https = {
+    port            = 443
+    protocol        = "HTTPS"
+    certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
 
+    forward = {
+      target_group_key = "ex-instance"
+    }
+  }
+  
   target_groups = [
     {
       name_prefix      = "blog-"
