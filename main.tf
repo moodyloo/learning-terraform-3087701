153c52683871437f9987e82aaa80cefb9189d283
Ming Xuan Wu <32241723+moodyloo@users.noreply.github.com>
2024-02-29 01:26:36 +0000

    -

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ module "blog_alb" {
   subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
-  http_tcp_listeners = [
+  http_tcp_listeners = [ 
     {
       port               = 80
       protocol           = "HTTP"
