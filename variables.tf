48ed33d2db60255391df66c06791cccdd1b03221
Ming Xuan Wu <32241723+moodyloo@users.noreply.github.com>
2024-02-29 00:30:56 +0000

    -

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "asg_min" {
+  description = "Minimum instance count for the ASG"
+  default     = 1
+}
+
+variable "asg_max" {
+  description = "Maximum instance count for the ASG"
+  default     = 2
+}
